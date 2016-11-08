433741aec13131be4aa2424c345590d066f2c79e
OneLogicalMyth <liam@glanfield.co.uk>
2016-11-08 09:30:23 +0000

    Added SQL injection c# example

---
+++ SQL Injection Example/Default.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="CS" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        body
+        {
+            font-family: Arial;
+            font-size: 10pt;
+        }
+        table
+        {
+            border:1px solid #ccc;
+            border-collapse:collapse;
+        }
+        table th
+        {
+            background-color: #F7F7F7;
+            color: #333;
+            font-weight: bold;
+        }
+        table th, table td
+        {
+            padding: 5px;
+            border-color: #ccc;
+        }
+	#SearchLastName
+	{
+		display: block;
+		width: 75%;
+	}
+    </style>
+</head>
+<body>
+<h1>Client Database Search</h1>
+    <form id="form1" runat="server">
+	<p>Last name search: <asp:TextBox ID="SearchLastName" runat="server"></asp:TextBox></p>
+	<p><asp:Button ID="SubmitSearch" runat="server" Text="Search" /></p>
+
+	<p><asp:PlaceHolder  ID = "PlaceHolder1" runat="server" /></p>
+	
+    </form>
+</body>
+</html>
---
+++ SQL Injection Example/Default.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
+
+public partial class CS : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (this.IsPostBack)
+        {
+            //Populating a DataTable from database.
+            DataTable dt = this.GetData();
+
+            //Building an HTML string.
+            StringBuilder html = new StringBuilder();
+
+            //Table start.
+            html.Append("<table border = '1'>");
+
+            //Building the Header row.
+            html.Append("<tr>");
+            foreach (DataColumn column in dt.Columns)
+            {
+                html.Append("<th>");
+                html.Append(column.ColumnName);
+                html.Append("</th>");
+            }
+            html.Append("</tr>");
+
+            //Building the Data rows.
+            foreach (DataRow row in dt.Rows)
+            {
+                html.Append("<tr>");
+                foreach (DataColumn column in dt.Columns)
+                {
+                    html.Append("<td>");
+                    html.Append(row[column.ColumnName]);
+                    html.Append("</td>");
+                }
+                html.Append("</tr>");
+            }
+
+            //Table end.
+            html.Append("</table>");
+
+            //Append the HTML string to Placeholder.
+            PlaceHolder1.Controls.Add(new LiteralControl(html.ToString()));
+        }
+    }
+
+    private DataTable GetData()
+    {
+	string WhereText = SearchLastName.Text;
+        string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+        using (SqlConnection con = new SqlConnection(constr))
+        {
+            using (SqlCommand cmd = new SqlCommand("SELECT TOP 10 Givenname, Surname, EmailAddress FROM Data WHERE Surname LIKE '%" + WhereText + "%'"))
+            {
+                using (SqlDataAdapter sda = new SqlDataAdapter())
+                {
+                    cmd.Connection = con;
+                    sda.SelectCommand = cmd;
+                    using (DataTable dt = new DataTable())
+                    {
+                        sda.Fill(dt);
+                        return dt;
+                    }
+                }
+            }
+        }
+    }
+}
