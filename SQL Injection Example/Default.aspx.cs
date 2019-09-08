3b2907eae16ba7a81e5786ce00be2dc1d4316f33
Liam Glanfield <liam@glanfield.co.uk>
2019-09-08 18:31:47 +0100

    Update Default.aspx.cs

--- SQL Injection Example/Default.aspx.cs
+++ SQL Injection Example/Default.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Web;
 using System.Web.UI;
@@ -17,61 +17,74 @@ public partial class CS : System.Web.UI.Page
             //Populating a DataTable from database.
             DataTable dt = this.GetData();
 
-            //Building an HTML string.
-            StringBuilder html = new StringBuilder();
-
-            //Table start.
-            html.Append("<table border = '1'>");
-
-            //Building the Header row.
-            html.Append("<tr>");
-            foreach (DataColumn column in dt.Columns)
+            if(dt != null)
             {
-                html.Append("<th>");
-                html.Append(column.ColumnName);
-                html.Append("</th>");
-            }
-            html.Append("</tr>");
+                //Building an HTML string.
+                StringBuilder html = new StringBuilder();
 
-            //Building the Data rows.
-            foreach (DataRow row in dt.Rows)
-            {
+                //Table start.
+                html.Append("<table border = '1'>");
+
+                //Building the Header row.
                 html.Append("<tr>");
                 foreach (DataColumn column in dt.Columns)
                 {
-                    html.Append("<td>");
-                    html.Append(row[column.ColumnName]);
-                    html.Append("</td>");
+                    html.Append("<th>");
+                    html.Append(column.ColumnName);
+                    html.Append("</th>");
                 }
                 html.Append("</tr>");
+
+                //Building the Data rows.
+                foreach (DataRow row in dt.Rows)
+                {
+                    html.Append("<tr>");
+                    foreach (DataColumn column in dt.Columns)
+                    {
+                        html.Append("<td>");
+                        html.Append(row[column.ColumnName]);
+                        html.Append("</td>");
+                    }
+                    html.Append("</tr>");
+                }
+
+                //Table end.
+                html.Append("</table>");
+
+                //Append the HTML string to Placeholder.
+                PlaceHolder1.Controls.Add(new LiteralControl(html.ToString()));
             }
-
-            //Table end.
-            html.Append("</table>");
-
-            //Append the HTML string to Placeholder.
-            PlaceHolder1.Controls.Add(new LiteralControl(html.ToString()));
         }
     }
 
     private DataTable GetData()
     {
-	string WhereText = SearchLastName.Text;
+    string WhereText = SearchLastName.Text;
         string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
         using (SqlConnection con = new SqlConnection(constr))
         {
-            using (SqlCommand cmd = new SqlCommand("SELECT TOP 10 Givenname, Surname, EmailAddress FROM Data WHERE Surname LIKE '%" + WhereText + "%'"))
+            try
             {
-                using (SqlDataAdapter sda = new SqlDataAdapter())
+                using (SqlCommand cmd = new SqlCommand("SELECT TOP 10 Givenname, Surname, EmailAddress FROM Data WHERE Surname LIKE '%" + WhereText + "%'"))
                 {
-                    cmd.Connection = con;
-                    sda.SelectCommand = cmd;
-                    using (DataTable dt = new DataTable())
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
                     {
-                        sda.Fill(dt);
-                        return dt;
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            return dt;
+                        }
                     }
                 }
+            }
+            catch (SqlException e)
+            {
+                StringBuilder html = new StringBuilder();
+                html.Append("<p>FAILED - <span style=color:red>" + e.Message + "</span></p>");
+                PlaceHolder1.Controls.Add(new LiteralControl(html.ToString()));
+                return null;
             }
         }
     }
